8c60289bc3351f49ab9726633e8c93f1c896d842
iedenis <denis.ievlev@gmail.com>
2019-01-12 04:28:15 +0200

    init

---
+++ index.js
@@ -0,0 +1,100 @@
+const TelegramBot = require('node-telegram-bot-api');
+var algorithmia = require("algorithmia");
+const fs = require('fs');
+var client = algorithmia("simDnQUsV485bmlVL1Sa+Vy61Dt1");
+const Axios = require('axios');
+const Path = require('path')
+const token = '';
+
+const bot = new TelegramBot(token, { polling: true });
+var file_to_save;
+var save_file;
+
+var nlp_directory = client.dir("data://ie_denis/nlp_directory")
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
+
+function fileDownload(response, remote_file, callback) {
+  console.log('response is: ' + response)
+  console.log('remote_file is: ' + remote_file)
+  client.file(remote_file).exists(function (exists) {
+    // Download contents of file as a string if it exists
+    client.file(remote_file).get(function (err, data) {
+      if (err) {
+        console.log("Failed to download file.");
+        console.log(err);
+      } else {
+        console.log("Successfully downloaded data.")
+      }
+
+      var input = data;
+
+      client.algo("deeplearning/ColorfulImageColorization/1.1.13")
+        .pipe(input)
+        .then(function (response) {
+          file_to_save = response.get().output;
+          // saveFile(file_to_save);
+          client.file(file_to_save).get(function (err, data) {
+            if (err) console.log(err)
+            else {
+              save_file = Math.random().toString(36).substring(7) + '.png';
+
+              fs.writeFileSync('./files/' + save_file, data);
+              callback(save_file);
+            }
+          })
+        });
+    });
+  });
+}
+
+function processFile(path, callback) {
+  const image_name = path.split('/').pop();
+  console.log('the image is: ' + image_name)
+  var remote_file = 'data://ie_denis/nlp_directory/' + image_name;
+  //console.log(path);
+  client.file(remote_file).exists(function (exists) {
+    // Check if file exists, if it doesn't create it
+    if (exists == false) {
+      nlp_directory.putFile(path, function (response) {
+        if (response.error) {
+          return console.log("Failed to upload file: " + response.error.message);
+        }
+        console.log("File uploaded. " + JSON.stringify(response));
+        fileDownload(response, remote_file, callback);
+      });
+    } else {
+      console.log("Your file already exists.")
+    }
+  });
+}
+
+
+//Listening
+bot.on('polling_error', error => console.log(error))
+
+
+bot.on('message', (msg) => {
+  var filePath;
+  const chatId = msg.chat.id;
+  if (typeof msg.photo == 'undefined') {
+    bot.sendMessage(chatId, "Hi, please upload a photo!")
+  }
+  else {
+    const photo = msg.photo;
+    const fileId = photo[2].file_id;
+    bot.downloadFile(fileId, __dirname + '/images/')
+      .then(path => processFile(path, function (save_file) {
+        // console.log('Sending photo '+ __dirname+'/files/'+save_file)
+        var photo = __dirname + '/files/' + save_file
+        bot.sendPhoto(chatId, photo, { caption: 'This is your colorized picture' })
+      }))
+      .catch(err => console.log(err));
+  }
+});
